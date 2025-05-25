5bbafcacec916198543b59cfa9779f0195c2a57f
Rhoda Ugboro <rhodaugboro@gmail.com>
2025-05-25 13:53:42 +0100

    New changes added

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,5 @@
-let arVal = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let arrVal = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-arVal.forEach(item => {
-    console.log(item)
+arrVal.forEach(list => {
+    console.log(list)
 });
